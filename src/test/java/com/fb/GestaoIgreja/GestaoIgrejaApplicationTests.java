7000ccfb2fea68a922538606912b498e25dc3608
FernandoBarboza <fernando.barboza18@gmail.com>
2025-05-13 19:56:46 -0300

    first commit

---
+++ src/test/java/com/fb/GestaoIgreja/GestaoIgrejaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fb.GestaoIgreja;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoIgrejaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
